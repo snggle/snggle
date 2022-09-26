71df4f0ba3b6e6e0ed6e68741eb1f72a937c7884
kronikarz <kronikarz91@protonmail.com>
2022-09-26 13:04:17 +0200

    Feature: Initial setup
    List of changes:
    - initialized empty flutter project
    - created analysis_options.yaml containing code rules used in project
    - created basic directory structure
    - added core libraries to pubspec.yaml (auto_route, equatable, flutter_bloc, get_it, intl, logger, build_runner)
    - updated README.md and .gitignore

---
+++ lib/shared/router/router.gr.dart
@@ -0,0 +1,54 @@
+// **************************************************************************
+// AutoRouteGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouteGenerator
+// **************************************************************************
+//
+// ignore_for_file: type=lint
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:auto_route/auto_route.dart' as _i2;
+import 'package:flutter/material.dart' as _i3;
+
+import '../../views/pages/pages_wrapper.dart' as _i1;
+
+class AppRouter extends _i2.RootStackRouter {
+  AppRouter([_i3.GlobalKey<_i3.NavigatorState>? navigatorKey])
+      : super(navigatorKey);
+
+  @override
+  final Map<String, _i2.PageFactory> pagesMap = {
+    RoutesWrapper.name: (routeData) {
+      return _i2.CustomPage<void>(
+        routeData: routeData,
+        child: const _i1.PagesWrapper(),
+        opaque: true,
+        barrierDismissible: false,
+      );
+    }
+  };
+
+  @override
+  List<_i2.RouteConfig> get routes => [
+        _i2.RouteConfig(
+          RoutesWrapper.name,
+          path: '/',
+        )
+      ];
+}
+
+/// generated route for
+/// [_i1.PagesWrapper]
+class RoutesWrapper extends _i2.PageRouteInfo<void> {
+  const RoutesWrapper()
+      : super(
+          RoutesWrapper.name,
+          path: '/',
+        );
+
+  static const String name = 'RoutesWrapper';
+}
---
+++ lib/views/pages/pages_wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PagesWrapper extends StatelessWidget {
+  const PagesWrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('Hello Snuggle'),
+    );
+  }
+}
