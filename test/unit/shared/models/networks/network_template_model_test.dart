79c53e63b150c7f1972071683a2d5fe3f403048b
Kamil Sobonski <39964811+SucharMistrz@users.noreply.github.com>
2025-09-11 14:59:33 +0200

    Improvements after the 1st CR by Olga

--- test/unit/shared/models/networks/network_template_model_test.dart
+++ test/unit/shared/models/networks/network_template_model_test.dart
@@ -8,7 +8,7 @@ import 'package:snggle/shared/models/networks/network_type.dart';
 
 void main() {
   group('Tests of NetworkTemplateModel.deriveWallet()', () {
-    test('Should [return AHDWallet] derived from specified Mnemonic and derivation path', () async {
+    test('Should [return AHDWallet] derived from specified Mnemonic and derivation path (Ethereum)', () async {
       // Arrange
       NetworkTemplateModel actualNetworkTemplateModel = NetworkTemplateModel(
         name: 'Ethereum1',
@@ -44,8 +44,65 @@ void main() {
           curveType: CurveType.secp256k1,
         ),
         privateKey: Secp256k1PrivateKey.fromBytes(base64Decode('I7S5jRH/GJjsK5uQAS+BVCcZgU3ytjjH1P+zPzjtRS4='), metadata: expectedBip32KeyMetadata),
-        publicKey: Secp256k1PublicKey.fromCompressedBytes(base64Decode('AlsnWtgkUnraj3YGBmwpOnNV8rh94AISDgIbpjYAjAOK'), metadata: expectedBip32KeyMetadata),
+        publicKey:
+            Secp256k1PublicKey.fromCompressedBytes(base64Decode('AlsnWtgkUnraj3YGBmwpOnNV8rh94AISDgIbpjYAjAOK'), metadata: expectedBip32KeyMetadata),
         derivationPath: LegacyDerivationPath.parse("m/44'/60'/0'/0/0"),
+      );
+
+      expect(actualHDWallet, expectedHDWallet);
+    });
+
+    test('Should [return AHDWallet] derived from specified Mnemonic and derivation path (Solana)', () async {
+      // Arrange
+      NetworkTemplateModel actualNetworkTemplateModel = NetworkTemplateModel(
+        name: 'Solana1',
+        networkIconType: NetworkIconType.solana,
+        derivationPathTemplate: "m/44'/501'/{{i}}'/0'",
+        addressEncoder: SolanaAddressEncoder(),
+        derivator: ED25519Derivator(),
+        curveType: CurveType.ed25519,
+        walletType: WalletType.legacy,
+        networkType: NetworkType.solana,
+      );
+
+      Mnemonic actualMnemonic = Mnemonic.fromMnemonicPhrase('carry pave input birth pole vague elephant moment either science food donkey');
+      String derivationPath = "m/44'/501'/0'/0'";
+
+      // Act
+      AHDWallet actualHDWallet = await actualNetworkTemplateModel.deriveWallet(actualMnemonic, derivationPath);
+
+      // Assert
+      Bip32KeyMetadata expectedBip32KeyMetadata = Bip32KeyMetadata(
+          depth: 4,
+          chainCode: base64Decode('b0mgOzeBnuIp+FaVuL4YjezjJ+usm05nRtJYmgCQZpo='),
+          fingerprint: BigInt.parse('1028205064'),
+          parentFingerprint: BigInt.parse('314124849'),
+          masterFingerprint: BigInt.parse('3227734419'),
+          shiftedIndex: 2147483648);
+
+      AHDWallet expectedHDWallet = LegacyHDWallet(
+        address: 'Df5B7D6cjsTWLHJ8v7vpjADonpMq3CPkwj4HyKDGCdnR',
+        walletConfig: LegacyWalletConfig<ABip32PrivateKey>(
+          addressEncoder: SolanaAddressEncoder(),
+          derivator: ED25519Derivator(),
+          curveType: CurveType.ed25519,
+        ),
+        privateKey: ED25519PrivateKey(
+          edPrivateKey: EDPrivateKey.fromBytes(
+            base64Decode('mFLUcbO000R/iyq+9IgwanAQuKkY16ETTfaFZGABRJ8='),
+          ),
+          metadata: expectedBip32KeyMetadata,
+        ),
+        publicKey: ED25519PublicKey(
+          edPublicKey: EDPublicKey(
+            EDPoint.fromBytes(
+              CurvePoints.generatorED25519,
+              base64Decode('vA0IoUxJf992+1mNyZNe/UOfcaX1lHtwjMGMbyjzTQI='),
+            ),
+          ),
+          metadata: expectedBip32KeyMetadata,
+        ),
+        derivationPath: LegacyDerivationPath.parse("m/44'/501'/0'/0'"),
       );
 
       expect(actualHDWallet, expectedHDWallet);
@@ -53,7 +110,7 @@ void main() {
   });
 
   group('Tests of NetworkTemplateModel.getCustomizableDerivationPath()', () {
-    test('Should [return customizable derivation path] if [variables EXIST]', () {
+    test('Should [return customizable derivation path] if [variables EXIST] (Ethereum)', () {
       // Arrange
       NetworkTemplateModel actualNetworkTemplateModel = NetworkTemplateModel(
         name: 'Ethereum1',
@@ -75,7 +132,29 @@ void main() {
       expect(actualDerivationPath, expectedDerivationPath);
     });
 
-    test('Should [return customizable derivation path] with added "address" section if [variables NOT EXIST]', () {
+    test('Should [return customizable derivation path] if [variables EXIST] (Solana)', () {
+      // Arrange
+      NetworkTemplateModel actualNetworkTemplateModel = NetworkTemplateModel(
+        name: 'Solana1',
+        networkIconType: NetworkIconType.solana,
+        derivationPathTemplate: "m/44'/501'/{{i}}'/{{y}}'",
+        addressEncoder: SolanaAddressEncoder(),
+        derivator: ED25519Derivator(),
+        curveType: CurveType.ed25519,
+        walletType: WalletType.legacy,
+        networkType: NetworkType.solana,
+      );
+
+      // Act
+      String actualDerivationPath = actualNetworkTemplateModel.getCustomizableDerivationPath(accountIndex: 1, changeIndex: 2, addressIndex: 3);
+
+      // Assert
+      String expectedDerivationPath = "3'/2'";
+
+      expect(actualDerivationPath, expectedDerivationPath);
+    });
+
+    test('Should [return customizable derivation path] with added "address" section if [variables NOT EXIST] (EThereum)', () {
       // Arrange
       NetworkTemplateModel actualNetworkTemplateModel = NetworkTemplateModel(
         name: 'Ethereum1',
@@ -96,10 +175,32 @@ void main() {
 
       expect(actualDerivationPath, expectedDerivationPath);
     });
+
+    test('Should [return customizable derivation path] with added "address" section if [variables NOT EXIST] (Solana)', () {
+      // Arrange
+      NetworkTemplateModel actualNetworkTemplateModel = NetworkTemplateModel(
+        name: 'Solana1',
+        networkIconType: NetworkIconType.solana,
+        derivationPathTemplate: "m/44'/501'/0'/0'",
+        addressEncoder: SolanaAddressEncoder(),
+        derivator: ED25519Derivator(),
+        curveType: CurveType.ed25519,
+        walletType: WalletType.legacy,
+        networkType: NetworkType.solana,
+      );
+
+      // Act
+      String actualDerivationPath = actualNetworkTemplateModel.getCustomizableDerivationPath(accountIndex: 1, changeIndex: 2, addressIndex: 3);
+
+      // Assert
+      String expectedDerivationPath = '3';
+
+      expect(actualDerivationPath, expectedDerivationPath);
+    });
   });
 
   group('Tests of NetworkTemplateModel.mergeCustomDerivationPath()', () {
-    test('Should [return derivation path] combined from base and customizable parts', () {
+    test('Should [return Ethereum derivation path] combined from base and customizable parts', () {
       // Arrange
       NetworkTemplateModel actualNetworkTemplateModel = NetworkTemplateModel(
         name: 'Ethereum1',
@@ -121,7 +222,29 @@ void main() {
       expect(actualDerivationPath, expectedDerivationPath);
     });
 
-    test('Should [return derivation path] combined from base and customizable parts (customizable part starts with /)', () {
+    test('Should [return Solana derivation path] combined from base and customizable parts', () {
+      // Arrange
+      NetworkTemplateModel actualNetworkTemplateModel = NetworkTemplateModel(
+        name: 'Solana1',
+        networkIconType: NetworkIconType.solana,
+        derivationPathTemplate: "m/44'/501'/{{i}}'/0'",
+        addressEncoder: SolanaAddressEncoder(),
+        derivator: ED25519Derivator(),
+        curveType: CurveType.ed25519,
+        walletType: WalletType.legacy,
+        networkType: NetworkType.solana,
+      );
+
+      // Act
+      String actualDerivationPath = actualNetworkTemplateModel.mergeCustomDerivationPath("1'/0'");
+
+      // Assert
+      String expectedDerivationPath = "m/44'/501'/1'/0'";
+
+      expect(actualDerivationPath, expectedDerivationPath);
+    });
+
+    test('Should [return Ethereum derivation path] combined from base and customizable parts (customizable part starts with /)', () {
       // Arrange
       NetworkTemplateModel actualNetworkTemplateModel = NetworkTemplateModel(
         name: 'Ethereum1',
@@ -143,7 +266,29 @@ void main() {
       expect(actualDerivationPath, expectedDerivationPath);
     });
 
-    test('Should [return derivation path] combined from base and customizable parts (customizable part ends with /)', () {
+    test('Should [return Solana derivation path] combined from base and customizable parts (customizable part starts with /)', () {
+      // Arrange
+      NetworkTemplateModel actualNetworkTemplateModel = NetworkTemplateModel(
+        name: 'Solana1',
+        networkIconType: NetworkIconType.solana,
+        derivationPathTemplate: "m/44'/501'/{{i}}'/0'",
+        addressEncoder: SolanaAddressEncoder(),
+        derivator: ED25519Derivator(),
+        curveType: CurveType.ed25519,
+        walletType: WalletType.legacy,
+        networkType: NetworkType.solana,
+      );
+
+      // Act
+      String actualDerivationPath = actualNetworkTemplateModel.mergeCustomDerivationPath("/1'/0'");
+
+      // Assert
+      String expectedDerivationPath = "m/44'/501'/1'/0'";
+
+      expect(actualDerivationPath, expectedDerivationPath);
+    });
+
+    test('Should [return Ethereum derivation path] combined from base and customizable parts (customizable part ends with /)', () {
       // Arrange
       NetworkTemplateModel actualNetworkTemplateModel = NetworkTemplateModel(
         name: 'Ethereum1',
@@ -165,7 +310,29 @@ void main() {
       expect(actualDerivationPath, expectedDerivationPath);
     });
 
-    test('Should [return derivation path] combined from base and customizable parts (customizable part empty)', () {
+    test('Should [return Solana derivation path] combined from base and customizable parts (customizable part ends with /)', () {
+      // Arrange
+      NetworkTemplateModel actualNetworkTemplateModel = NetworkTemplateModel(
+        name: 'Solana1',
+        networkIconType: NetworkIconType.solana,
+        derivationPathTemplate: "m/44'/501'/{{i}}'/0'",
+        addressEncoder: SolanaAddressEncoder(),
+        derivator: ED25519Derivator(),
+        curveType: CurveType.ed25519,
+        walletType: WalletType.legacy,
+        networkType: NetworkType.solana,
+      );
+
+      // Act
+      String actualDerivationPath = actualNetworkTemplateModel.mergeCustomDerivationPath("1'/0'/");
+
+      // Assert
+      String expectedDerivationPath = "m/44'/501'/1'/0'";
+
+      expect(actualDerivationPath, expectedDerivationPath);
+    });
+
+    test('Should [return Ethereum derivation path] combined from base and customizable parts (customizable part empty)', () {
       // Arrange
       NetworkTemplateModel actualNetworkTemplateModel = NetworkTemplateModel(
         name: 'Ethereum1',
@@ -186,10 +353,32 @@ void main() {
 
       expect(actualDerivationPath, expectedDerivationPath);
     });
+
+    test('Should [return Solana derivation path] combined from base and customizable parts (customizable part empty)', () {
+      // Arrange
+      NetworkTemplateModel actualNetworkTemplateModel = NetworkTemplateModel(
+        name: 'Solana1',
+        networkIconType: NetworkIconType.solana,
+        derivationPathTemplate: "m/44'/501'/{{i}}'/0'",
+        addressEncoder: SolanaAddressEncoder(),
+        derivator: ED25519Derivator(),
+        curveType: CurveType.ed25519,
+        walletType: WalletType.legacy,
+        networkType: NetworkType.solana,
+      );
+
+      // Act
+      String actualDerivationPath = actualNetworkTemplateModel.mergeCustomDerivationPath('');
+
+      // Assert
+      String expectedDerivationPath = "m/44'/501'";
+
+      expect(actualDerivationPath, expectedDerivationPath);
+    });
   });
 
   group('Tests of NetworkTemplateModel.baseDerivationPath getter', () {
-    test('Should [return String] representing static part of derivation path (when derivation path template CONTAINS dynamic elements)', () {
+    test('Should [return String] representing static part of Ethereum derivation path (when derivation path template CONTAINS dynamic elements)', () {
       // Arrange
       NetworkTemplateModel actualNetworkTemplateModel = NetworkTemplateModel(
         name: 'Ethereum1',
@@ -211,7 +400,30 @@ void main() {
       expect(actualBaseDerivationPath, expectedBaseDerivationPath);
     });
 
-    test('Should [return String] representing static part of derivation path (when derivation path template NOT CONTAINS dynamic elements)', () {
+    test('Should [return String] representing static part of Solana derivation path (when derivation path template CONTAINS dynamic elements)', () {
+      // Arrange
+      NetworkTemplateModel actualNetworkTemplateModel = NetworkTemplateModel(
+        name: 'Solana1',
+        networkIconType: NetworkIconType.solana,
+        derivationPathTemplate: "m/44'/501'/{{i}}'/0'",
+        addressEncoder: SolanaAddressEncoder(),
+        derivator: ED25519Derivator(),
+        curveType: CurveType.ed25519,
+        walletType: WalletType.legacy,
+        networkType: NetworkType.solana,
+      );
+
+      // Act
+      String actualBaseDerivationPath = actualNetworkTemplateModel.baseDerivationPath;
+
+      // Assert
+      String expectedBaseDerivationPath = "m/44'/501'";
+
+      expect(actualBaseDerivationPath, expectedBaseDerivationPath);
+    });
+
+    test('Should [return String] representing static part of Ethereum derivation path (when derivation path template NOT CONTAINS dynamic elements)',
+        () {
       // Arrange
       NetworkTemplateModel actualNetworkTemplateModel = NetworkTemplateModel(
         name: 'Ethereum1',
@@ -233,7 +445,30 @@ void main() {
       expect(actualBaseDerivationPath, expectedBaseDerivationPath);
     });
 
-    test('Should [return String] representing static part of derivation path (when derivation path template EMPTY)', () {
+    test('Should [return String] representing static part of Solana derivation path (when derivation path template NOT CONTAINS dynamic elements)',
+        () {
+      // Arrange
+      NetworkTemplateModel actualNetworkTemplateModel = NetworkTemplateModel(
+        name: 'Solana1',
+        networkIconType: NetworkIconType.solana,
+        derivationPathTemplate: "m/44'/501'/0'/0'",
+        addressEncoder: SolanaAddressEncoder(),
+        derivator: ED25519Derivator(),
+        curveType: CurveType.ed25519,
+        walletType: WalletType.legacy,
+        networkType: NetworkType.solana,
+      );
+
+      // Act
+      String actualBaseDerivationPath = actualNetworkTemplateModel.baseDerivationPath;
+
+      // Assert
+      String expectedBaseDerivationPath = "m/44'/501'/0'/0'";
+
+      expect(actualBaseDerivationPath, expectedBaseDerivationPath);
+    });
+
+    test('Should [return String] representing static part of Ethereum derivation path (when derivation path template EMPTY)', () {
       // Arrange
       NetworkTemplateModel actualNetworkTemplateModel = NetworkTemplateModel(
         name: 'Ethereum1',
@@ -244,6 +479,28 @@ void main() {
         curveType: CurveType.secp256k1,
         walletType: WalletType.legacy,
         networkType: NetworkType.ethereum,
+      );
+
+      // Act
+      String actualBaseDerivationPath = actualNetworkTemplateModel.baseDerivationPath;
+
+      // Assert
+      String expectedBaseDerivationPath = 'm';
+
+      expect(actualBaseDerivationPath, expectedBaseDerivationPath);
+    });
+
+    test('Should [return String] representing static part of Solana derivation path (when derivation path template EMPTY)', () {
+      // Arrange
+      NetworkTemplateModel actualNetworkTemplateModel = NetworkTemplateModel(
+        name: 'Solana1',
+        networkIconType: NetworkIconType.solana,
+        derivationPathTemplate: 'm',
+        addressEncoder: SolanaAddressEncoder(),
+        derivator: ED25519Derivator(),
+        curveType: CurveType.ed25519,
+        walletType: WalletType.legacy,
+        networkType: NetworkType.solana,
       );
 
       // Act
--- test/unit/shared/utils/string_utils_test.dart
+++ test/unit/shared/utils/string_utils_test.dart
@@ -3,31 +3,57 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:snggle/shared/utils/string_utils.dart';
 
 void main() {
-  group('Tests of StringUtils.getShortText()', () {
-    test('Should [return short HEX string] if given [value IS HEX]', () {
+  group('Tests of StringUtils.getShortPublicAddress()', () {
+    test('Should [return short public address HEX string] if given [value IS HEX (Ethereum address)]', () {
       // Arrange
-      String actualHex = '0x16980b3B4a3f9D89E33311B5aa8f80303E5ca4F8';
+      String actualPublicAddressHex = '0x16980b3B4a3f9D89E33311B5aa8f80303E5ca4F8';
 
       // Act
-      String actualShortHex = StringUtils.getShortPublicAddress(actualHex, 3);
+      String actualShortPublicAddressHex = StringUtils.getShortPublicAddress(actualPublicAddressHex, 3);
 
       // Assert
-      String expectedShortHex = '0x169...4F8';
+      String expectedShortPublicAddressHex = '0x169...4F8';
 
-      expect(actualShortHex, expectedShortHex);
+      expect(actualShortPublicAddressHex, expectedShortPublicAddressHex);
     });
 
-    test('Should [return short text string] if given [value NOT HEX]', () {
+    test('Should [return short public address text string] if given [value NOT HEX (Solana address)]', () {
       // Arrange
-      String actualHex = 'Hello, World!';
+      String actualPublicAddress = '2xGD7cWtwpmCpW2NvT9EJt96eDavS3suVgQNVaBU4A19';
 
       // Act
-      String actualShortHex = StringUtils.getShortPublicAddress(actualHex, 3);
+      String actualShortPublicAddress = StringUtils.getShortPublicAddress(actualPublicAddress, 3);
 
       // Assert
-      String expectedShortHex = 'Hel...ld!';
+      String expectedShortPublicAddress = '2xG...A19';
 
-      expect(actualShortHex, expectedShortHex);
+      expect(actualShortPublicAddress, expectedShortPublicAddress);
+    });
+
+    test('Should [return short public address text string] if given [value NOT HEX (Bitcoin address)]', () {
+      // Arrange
+      String actualPublicAddress = 'bc1qxy2kgdygjrsqtzq2n0yrf2493p83kkfjhx0wlh';
+
+      // Act
+      String actualShortPublicAddress = StringUtils.getShortPublicAddress(actualPublicAddress, 3);
+
+      // Assert
+      String expectedShortPublicAddress = 'bc1...wlh';
+
+      expect(actualShortPublicAddress, expectedShortPublicAddress);
+    });
+
+    test('Should [return short text string] if given [value NOT HEX (String)]', () {
+      // Arrange
+      String actualString = 'Hello, World!';
+
+      // Act
+      String actualShortString = StringUtils.getShortPublicAddress(actualString, 3);
+
+      // Assert
+      String expectedShortString = 'Hel...ld!';
+
+      expect(actualShortString, expectedShortString);
     });
   });
 
