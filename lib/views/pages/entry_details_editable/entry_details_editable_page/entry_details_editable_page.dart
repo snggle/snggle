8408fca3f9d13a7d28d2866a1c0d7176e4395caa
Kamil Sobonski <39964811+SucharMistrz@users.noreply.github.com>
2026-06-12 09:57:35 +0200

    [drop] Feature: Password Manager Entries UI
    This branch introduces the UI for the Password Manager component in a form of Entries, the second main class of items alongside Vaults. The empty Secrets page in the application was replaced with the new view with the list of entries, where users may create, edit, view, and manage credentials containing usernames, emails, and passwords. Each entry icon is a 2x2 grid which in turn contains icons representing the types of information an entry stores, with separate icons for: email, username, password.
    
    List of changes:
    - created details_hide.svg and details_show.svg icons and added them to app_icons.dart, they are used for buttons which toggle password visibility in entries
    - created icon_entry_email.svg, icon_entry_password.svg and icon_entry_username.svg icons and added them to app_icons.dart, they are used to outline what sort of data each entry contains
    - created entry_details_page.dart, containing read-only entry contents, with a support for copy-on-click for individual entry details
    - created entry_details_editable_page.dart, containing editable entry contents, where a user may modify entry details
    - created entry_page_type.dart enum, used to differentiate between creating a new entry or modifying an existing one
    - created entry_create_edit_status.dart enum, used to display the correct message in a dialog after successfully creating or modifying an entry
    - created entry_list_page.dart and entry_list_page_cubit.dart, adding the list of entries view
    - created entry_list_item.dart, defining the appearance of the entries on the list of entries view
    - created entry_icon.dart, implementing Entry icons with their content preview
    - created custom_clipboard.dart, implementing method channel which allows copying data and obscuring the clipboard content preview
    - updated MainActivity.kt, adding method channel which allows copying data and obscuring the clipboard content preview
    - updated a_list_cubit.dart, network_list_page_cubit.dart, vault_list_page_cubit.dart and wallet_list_page_cubit.dart, allowing item renaming to be set not only on a per-list basis, but to also take to item type into account
    - updated router.dart, adding new routes for Entries
    - updated container_icon_grid.dart, container_icon_grid_item.dart and list_item_icon.dart, implementing support for Entry icons and their content preview
    - updated group_icon.dart, network_group_icon.dart and vault_icon.dart with changes in container_icon_grid.dart
    - updated custom_text_field.dart, adding a bool to support both static (used in entry_details_page.dart and entry_details_editable_page.dart) and dynamically moving suffixes (used in wallet_create_page.dart), as well as adding onTap callback required for copying to work correctly when CustomTextField is wrapped in CopyWrapper
    - updated wallet_create_page.dart, ensuring Solana derivation path keeps using dynamic suffix after changes in custom_text_field.dart
    - updated copy_wrapper.dart, adding a callback required for copying to work correctly when CustomTextField is wrapped in CopyWrapper, and adding support for copying data with obscured clipboard content preview
    - updated ethereum_transaction_details_page.dart, solana_transaction_details_page.dart, transaction_list_item_expansion.dart, wallet_details_page.dart, abi_chunks_list_item.dart and public_address_preview.dart with changes to copy_wrapper.dart

---
+++ lib/views/pages/entry_details_editable/entry_details_editable_page/entry_details_editable_page.dart
@@ -0,0 +1,214 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:snggle/bloc/pages/entry_details_editable/entry_details_editable_page/entry_details_editable_page_cubit.dart';
+import 'package:snggle/bloc/pages/entry_details_editable/entry_details_editable_page/entry_details_editable_page_state.dart';
+import 'package:snggle/bloc/pages/entry_details_editable/entry_page_type.dart';
+import 'package:snggle/config/app_colors.dart';
+import 'package:snggle/config/app_icons/app_icons.dart';
+import 'package:snggle/shared/models/entries/entry_model.dart';
+import 'package:snggle/shared/utils/filesystem_path.dart';
+import 'package:snggle/views/pages/bottom_navigation/entries_wrapper/entry_list_page/entry_create_edit_status.dart';
+import 'package:snggle/views/widgets/custom/custom_scaffold.dart';
+import 'package:snggle/views/widgets/custom/custom_text_field.dart';
+import 'package:snggle/views/widgets/custom/dialog/custom_loading_dialog.dart';
+import 'package:snggle/views/widgets/generic/error_message_list_tile.dart';
+import 'package:snggle/views/widgets/generic/label_wrapper_vertical.dart';
+import 'package:snggle/views/widgets/generic/scrollable_layout.dart';
+import 'package:snggle/views/widgets/icons/asset_icon.dart';
+import 'package:snggle/views/widgets/keyboard/keyboard_value_notifier.dart';
+import 'package:snggle/views/widgets/keyboard/keyboard_visibility_builder.dart';
+import 'package:snggle/views/widgets/tooltip/bottom_tooltip/bottom_tooltip_item.dart';
+
+@RoutePage()
+class EntryDetailsEditablePage extends StatefulWidget {
+  final FilesystemPath? parentFilesystemPath;
+  final EntryModel? entryModel;
+  final EntryPageType entryPageType;
+  final bool? obscurePasswordBool;
+
+  const EntryDetailsEditablePage({
+    required this.entryPageType,
+    this.parentFilesystemPath,
+    this.entryModel,
+    this.obscurePasswordBool = true,
+    super.key,
+  });
+
+  @override
+  State<StatefulWidget> createState() => _EntryDetailsEditablePageState();
+}
+
+class _EntryDetailsEditablePageState extends State<EntryDetailsEditablePage> {
+  final ScrollController scrollController = ScrollController();
+  final KeyboardValueNotifier keyboardValueNotifier = KeyboardValueNotifier();
+
+  late bool _obscurePasswordBool;
+
+  late final EntryDetailsEditablePageCubit entryDetailsEditablePageCubit = EntryDetailsEditablePageCubit(
+    parentFilesystemPath: widget.parentFilesystemPath,
+    entryModel: widget.entryModel,
+    entryPageType: widget.entryPageType,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _obscurePasswordBool = widget.obscurePasswordBool ?? true;
+    entryDetailsEditablePageCubit.init();
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    keyboardValueNotifier.dispose();
+    entryDetailsEditablePageCubit.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
+
+    return BlocBuilder<EntryDetailsEditablePageCubit, EntryDetailsEditablePageState>(
+      bloc: entryDetailsEditablePageCubit,
+      builder: (BuildContext context, EntryDetailsEditablePageState state) {
+        return CustomScaffold(
+          title: switch (widget.entryPageType) {
+            EntryPageType.entryPageCreate => 'CREATE ENTRY',
+            EntryPageType.entryPageEdit => 'EDIT ENTRY',
+          },
+          resizeToAvoidBottomInsetBool: true,
+          body: KeyboardVisibilityBuilder(
+            keyboardValueNotifier: keyboardValueNotifier,
+            builder: ({required bool customKeyboardVisibleBool, required bool nativeKeyboardVisibleBool}) {
+              bool anyKeyboardVisibleBool = customKeyboardVisibleBool || nativeKeyboardVisibleBool;
+
+              return ScrollableLayout(
+                scrollController: scrollController,
+                bottomMarginVisibleBool: anyKeyboardVisibleBool == false,
+                tooltipVisibleBool: anyKeyboardVisibleBool == false,
+                tooltipItems: <Widget>[
+                  BottomTooltipItem(
+                    label: 'Save',
+                    assetIconData: AppIcons.menu_save,
+                    onTap: _finishButtonEnabledBool ? _save : null,
+                  ),
+                ],
+                child: SingleChildScrollView(
+                  controller: scrollController,
+                  child: Column(
+                    children: <Widget>[
+                      _buildEditableEntryField(
+                        textTheme: textTheme,
+                        label: 'Name',
+                        textEditingController: entryDetailsEditablePageCubit.nameTextEditingController,
+                      ),
+                      if (state.entryNameEmptyBool == true)
+                        const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 20),
+                          child: ErrorMessageListTile(
+                            message: 'Entry name cannot be empty',
+                          ),
+                        ),
+                      const SizedBox(height: 12),
+                      _buildEditableEntryField(
+                        textTheme: textTheme,
+                        label: 'Website',
+                        textEditingController: entryDetailsEditablePageCubit.websiteTextEditingController,
+                      ),
+                      const SizedBox(height: 12),
+                      _buildEditableEntryField(
+                        textTheme: textTheme,
+                        label: 'Email',
+                        textEditingController: entryDetailsEditablePageCubit.emailTextEditingController,
+                      ),
+                      const SizedBox(height: 12),
+                      _buildEditableEntryField(
+                        textTheme: textTheme,
+                        label: 'Username',
+                        textEditingController: entryDetailsEditablePageCubit.usernameTextEditingController,
+                      ),
+                      const SizedBox(height: 12),
+                      _buildEditableEntryField(
+                        textTheme: textTheme,
+                        label: 'Password',
+                        textEditingController: entryDetailsEditablePageCubit.passwordTextEditingController,
+                        obscureTextBool: _obscurePasswordBool,
+                        suffixWidget: InkWell(
+                          onTap: () => setState(() => _obscurePasswordBool = !_obscurePasswordBool),
+                          child: Padding(
+                            padding: const EdgeInsets.only(right: 6),
+                            child: AssetIcon(
+                              _obscurePasswordBool ? AppIcons.details_hide : AppIcons.details_show,
+                            ),
+                          ),
+                        ),
+                        suffixWidgetConstraints: const BoxConstraints(minWidth: 34, minHeight: 34),
+                      ),
+                      SizedBox(height: anyKeyboardVisibleBool ? 40 : 100),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildEditableEntryField({
+    required TextTheme textTheme,
+    required String label,
+    required TextEditingController textEditingController,
+    bool readOnlyBool = false,
+    bool autofocusBool = false,
+    bool obscureTextBool = false,
+    FocusNode? focusNode,
+    Widget? suffixWidget,
+    BoxConstraints? suffixWidgetConstraints,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: LabelWrapperVertical.textField(
+        label: label,
+        labelStyle: textTheme.bodyMedium?.copyWith(color: AppColors.darkGrey),
+        labelPadding: EdgeInsets.zero,
+        child: CustomTextField(
+          autofocusBool: autofocusBool,
+          readOnlyBool: readOnlyBool,
+          focusNode: focusNode,
+          enableInteractiveSelectionBool: true,
+          textEditingController: textEditingController,
+          inputBorder: InputBorder.none,
+          keyboardType: TextInputType.text,
+          padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 5),
+          obscureTextBool: obscureTextBool,
+          suffixWidget: suffixWidget,
+          suffixWidgetConstraints: suffixWidgetConstraints,
+        ),
+      ),
+    );
+  }
+
+  bool get _finishButtonEnabledBool => entryDetailsEditablePageCubit.state.entryNameEmptyBool == false;
+
+  Future<void> _save() async {
+    await CustomLoadingDialog.show<EntryModel?>(
+      context: context,
+      title: 'Saving...',
+      futureFunction: entryDetailsEditablePageCubit.save,
+      onSuccess: (EntryModel? entryModel) async {
+        if (entryModel != null) {
+          AutoRouter.of(context).root.pop(
+            switch (widget.entryPageType) {
+              EntryPageType.entryPageCreate => EntryCreateEditStatus.creationSuccessful,
+              EntryPageType.entryPageEdit => EntryCreateEditStatus.modificationSuccessful,
+            },
+          );
+        }
+      },
+    );
+  }
+}
